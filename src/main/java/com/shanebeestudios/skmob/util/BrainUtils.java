876550cc19aaa516e64e29650976a6c819ff5cbb
ShaneBee <shanebolenback@me.com>
2023-07-10 12:57:46 -0700

    BrainUtils - updates - merge getBrain methods (no need for 2) - add shortcut methods for getting EntityAI

--- src/main/java/com/shanebeestudios/skmob/util/BrainUtils.java
+++ src/main/java/com/shanebeestudios/skmob/util/BrainUtils.java
@@ -1,24 +1,35 @@
 package com.shanebeestudios.skmob.util;
 
 import me.gamercoder215.mobchip.EntityBrain;
+import me.gamercoder215.mobchip.ai.EntityAI;
 import me.gamercoder215.mobchip.bukkit.BukkitBrain;
 import org.bukkit.entity.EnderDragon;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Mob;
 import org.jetbrains.annotations.Nullable;
 
 public class BrainUtils {
 
     @Nullable
-    public static EntityBrain getBrain(Entity entity) {
-        if (entity instanceof EnderDragon enderDragon) return BukkitBrain.getBrain(enderDragon);
-        else if (entity instanceof Mob mob) return BukkitBrain.getBrain(mob);
+    public static EntityBrain getBrain(Object object) {
+        if (object instanceof EntityBrain entityBrain) return entityBrain;
+        if (object instanceof EnderDragon enderDragon) return BukkitBrain.getBrain(enderDragon);
+        else if (object instanceof Mob mob) return BukkitBrain.getBrain(mob);
         return null;
     }
 
-    public static EntityBrain getBrain(Object object) {
-        if (object instanceof EntityBrain entityBrain) return entityBrain;
-        else if (object instanceof Entity entity) return getBrain(entity);
+    @Nullable
+    public static EntityAI getGoalAI(Object object) {
+        if (object instanceof EntityAI entityAI) return entityAI;
+        EntityBrain brain = getBrain(object);
+        if (brain != null) return brain.getGoalAI();
+        return null;
+    }
+
+    @Nullable
+    public static EntityAI getTargetAI(Object object) {
+        if (object instanceof EntityAI entityAI) return entityAI;
+        EntityBrain brain = getBrain(object);
+        if (brain != null) return brain.getTargetAI();
         return null;
     }
 
